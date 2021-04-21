b9733e9a7b566443cbce915d0b52d0b9dd9c52a4
planeset94 <planeset94@gmail.com>
2021-04-21 15:54:30 +0200

    Added JS file with Sample

---
+++ assets/JS/myscript.js
@@ -0,0 +1,8 @@
+// document.getElementById('testo')='ciao a tutti';
+
+alert("Helloooo");
+
+
+// document.writeln("Hello World");
+document.getElementById("testo").innerHTML="Hello World";
+document.getElementById("bottone_id").innerHTML="Salve docenti boolean";
